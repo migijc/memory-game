64dd65e71993cc6286b0fa161d15d89baa4a53e2
Miguel <migijc@gmail.com>
2022-10-08 17:28:41 -0400

    Initial commit, previous directory was corrupt

---
+++ memory-app/src/App.js
@@ -0,0 +1,15 @@
+import CardsDisplay from "./components/CardDisplay";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cardDisplay">
+       <CardsDisplay/>
+    
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
